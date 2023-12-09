cc41a91fb28ec96b0c6e7d97527f2684591279b9
Hebert Costa Andrade <hcostapuc@gmail.com>
2023-12-09 14:26:33 -0300

    add washOrder validation

--- src/Architecture.Template.Application/WashOrder/Commands/CreateWashOrder/CreateWashOrderCommandValidator.cs
+++ src/Architecture.Template.Application/WashOrder/Commands/CreateWashOrder/CreateWashOrderCommandValidator.cs
@@ -6,11 +6,14 @@ public sealed class CreateWashOrderCommandValidator : AbstractValidator<CreateWa
 {
     private readonly IClientRepository _clientRepository;
     private readonly IVehicleRepository _vehicleRepository;
+    private readonly IWashOrderRepository _washOrderRepository;
     public CreateWashOrderCommandValidator(IClientRepository clientRepository,
-                                         IVehicleRepository vehicleRepository)
+                                         IVehicleRepository vehicleRepository,
+                                         IWashOrderRepository washOrderRepository)
     {
         _clientRepository = clientRepository ?? Guard.Against.Null(clientRepository, nameof(clientRepository));
         _vehicleRepository = vehicleRepository ?? Guard.Against.Null(vehicleRepository, nameof(vehicleRepository));
+        _washOrderRepository = washOrderRepository ?? Guard.Against.Null(washOrderRepository, nameof(washOrderRepository));
         
         RuleFor(v => v.ClientId)
             .NotEmpty().WithMessage("ClientId is required.")
@@ -18,16 +21,18 @@ public sealed class CreateWashOrderCommandValidator : AbstractValidator<CreateWa
 
         RuleFor(v => v.VehicleId)
             .NotEmpty().WithMessage("VehicleId is required.")
-            .MustAsync(BeExistVehicleAsync).WithMessage("The specified vehicle not exists.");
+            .MustAsync(BeExistVehicleAsync).WithMessage("The specified vehicle not exists.")
+          .MustAsync(BeUniqueWashAsync).WithMessage("Already exist a wash order opened to the specific vehicle.");
 
         RuleFor(v => new { v.ClientId, v.VehicleId })
           .MustAsync(BeValidCombineEntityAsync).WithMessage("The combination between client and vehicle must exist.");
-
     }
-    public async Task<bool> BeExistClientAsync(Guid clientId, CancellationToken cancellationToken) =>
-      await _clientRepository.ExistAsync(l => l.Id == clientId, cancellationToken);
-    public async Task<bool> BeExistVehicleAsync(Guid vehicleId, CancellationToken cancellationToken) =>
-      await _vehicleRepository.ExistAsync(l => l.Id == vehicleId, cancellationToken);
+    public async Task<bool> BeExistClientAsync(Guid id, CancellationToken cancellationToken) =>
+      await _clientRepository.ExistAsync(l => l.Id == id, cancellationToken);
+    public async Task<bool> BeExistVehicleAsync(Guid id, CancellationToken cancellationToken) =>
+      await _vehicleRepository.ExistAsync(l => l.Id == id, cancellationToken);
     public async Task<bool> BeValidCombineEntityAsync(CreateWashOrderCommand model, object _, CancellationToken cancellationToken) =>
       await _vehicleRepository.ExistAsync(l => l.ClientId == model.ClientId && l.Id == model.VehicleId, cancellationToken);
+    public async Task<bool> BeUniqueWashAsync(Guid id, CancellationToken cancellationToken) =>
+      !await _washOrderRepository.ExistAsync(l => l.VehicleId == id && l.Status == Domain.Enums.StatusOrder.Open, cancellationToken);
 }
